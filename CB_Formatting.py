0fd7c3c381a8e8e95a51a65c373290be8d0a681d
Seamus Mullan <24carrotgaming@gmail.com>
2021-10-10 15:00:14 +0100

    Added More Colours and made them a class

--- CB_Formatting.py
+++ CB_Formatting.py
@@ -2,26 +2,70 @@ import datetime
 import os
 
 # Things like text colors, etc.
-HEADER = '\033[95m'
-OKBLUE = '\033[94m'
-OKCYAN = '\033[96m'
-OKGREEN = '\033[92m'
-WARNING = '\033[93m'
-FAIL = '\033[91m'
-ENDC = '\033[0m'
-BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
+class C:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    CEND      = '\33[0m'
+    CBOLD     = '\33[1m'
+    CITALIC   = '\33[3m'
+    CURL      = '\33[4m'
+    CBLINK    = '\33[5m'
+    CBLINK2   = '\33[6m'
+    CSELECTED = '\33[7m'
+
+    CBLACK  = '\33[30m'
+    CRED    = '\33[31m'
+    CGREEN  = '\33[32m'
+    CYELLOW = '\33[33m'
+    CBLUE   = '\33[34m'
+    CVIOLET = '\33[35m'
+    CBEIGE  = '\33[36m'
+    CWHITE  = '\33[37m'
+
+    CBLACKBG  = '\33[40m'
+    CREDBG    = '\33[41m'
+    CGREENBG  = '\33[42m'
+    CYELLOWBG = '\33[43m'
+    CBLUEBG   = '\33[44m'
+    CVIOLETBG = '\33[45m'
+    CBEIGEBG  = '\33[46m'
+    CWHITEBG  = '\33[47m'
+
+    CGREY    = '\33[90m'
+    CRED2    = '\33[91m'
+    CGREEN2  = '\33[92m'
+    CYELLOW2 = '\33[93m'
+    CBLUE2   = '\33[94m'
+    CVIOLET2 = '\33[95m'
+    CBEIGE2  = '\33[96m'
+    CWHITE2  = '\33[97m'
+
+    CGREYBG    = '\33[100m'
+    CREDBG2    = '\33[101m'
+    CGREENBG2  = '\33[102m'
+    CYELLOWBG2 = '\33[103m'
+    CBLUEBG2   = '\33[104m'
+    CVIOLETBG2 = '\33[105m'
+    CBEIGEBG2  = '\33[106m'
+    CWHITEBG2  = '\33[107m'
 
 # Functions for dealing with error formatting
 
 def returnError(error):
-    print(FAIL + "ERROR: " + ENDC + "\n" + WARNING + error + ENDC)
+    print(C.FAIL + "ERROR: " + C.ENDC + "\n" + C.WARNING + error + C.ENDC)
 
 def returnWarning(warning):
-    print(WARNING + "WARNING: " + ENDC + "\n" + WARNING + warning + ENDC)
+    print(C.WARNING + "WARNING: " + C.ENDC + "\n" + C.WARNING + warning + C.ENDC)
 
 def returnInfo(info):
-    print(OKBLUE + "INFO: " + ENDC + "\n" + OKBLUE + info + ENDC)
+    print(C.OKBLUE + "INFO: " + C.ENDC + "\n" + C.OKBLUE + info + C.ENDC)
 
 def saveErrorLog(error):
     if not os.path.exists("Error Logs\\"):
@@ -35,9 +79,9 @@ def saveErrorLog(error):
 # BIG PARAGRAPH OF TEXT
 def returnTwintInfo():
     print(f"""
-    {OKBLUE}Here is how to format your variables for the twitter modules.{ENDC}
+    {C.OKBLUE}Here is how to format your variables for the twitter modules.{C.ENDC}
 
-    {OKCYAN}    Variable             Type       Description{OKGREEN}
+    {C.OKCYAN}    Variable             Type       Description{C.OKGREEN}
     --------------------------------------------
     Username             (string) - Twitter user's username
     User_id              (string) - Twitter user's user_id
@@ -101,7 +145,7 @@ def returnTwintInfo():
     Source               (string) - Filter the tweets for specific source client. (example: `--source "Twitter Web Client"`)
     Members_list         (string) - Filter the tweets sent by users in a given list.
     Filter_retweets      (bool)   - Exclude retweets from the results.
-    {ENDC}
+    {C.ENDC}
     """)
 
 
